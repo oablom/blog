30e4389e4b69979b0feb3dd3d67175507f5c7f22
Oa Blom <oablom@gmail.com>
2024-05-08 20:52:18 +0200

    feat: Add Category, Post, and User models and routes

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authenticationRoute = require("./api/routes/auth");
 
 dotenv.config();
 
@@ -17,6 +18,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+app.use("/api/auth", authRoute);
 
 app.listen("5000", () => {
   console.log("Backend server is running!");
