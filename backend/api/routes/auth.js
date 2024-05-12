aea8816595b02146d60e810f23395e490ffe3f9b
Oa Blom <oablom@gmail.com>
2024-05-12 22:31:02 +0200

    chore: Refactor auth.js to follow consistent naming conventions

--- backend/api/routes/auth.js
+++ backend/api/routes/auth.js
@@ -3,7 +3,7 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-//Register
+// Register
 router.post("/register", async (req, res) => {
   try {
     const salt = await bcrypt.genSalt(10);
